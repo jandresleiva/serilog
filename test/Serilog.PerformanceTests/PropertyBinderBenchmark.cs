bb13c7e13073851cd897649071592ba722dd6546
jandresleiva <andresleiva.4@gmail.com>
2021-11-08 20:36:43 -0300

    Adds BaseLine Benchmark checks

--- test/Serilog.PerformanceTests/PropertyBinderBenchmark.cs
+++ test/Serilog.PerformanceTests/PropertyBinderBenchmark.cs
@@ -1,4 +1,5 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using Serilog.Capturing;
 using Serilog.Core;
 using Serilog.Events;
@@ -12,12 +13,14 @@ using System.Threading.Tasks;
 namespace Serilog.PerformanceTests
 {
     [MemoryDiagnoser]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    [CategoriesColumn]
     public class PropertyBinderBenchmark
     {
         private PropertyBinder binder = new PropertyBinder(
                 new PropertyValueConverter(10, 1000, 1000, Enumerable.Empty<Type>(), Enumerable.Empty<IDestructuringPolicy>(), false));
 
-        [Benchmark]
+        [BenchmarkCategory("Positional"), Benchmark]
         public void CallPositionalConstructProperties()
         {
             var messageTemplate = "Hello {1} {0} nothing more";
@@ -26,8 +29,8 @@ namespace Serilog.PerformanceTests
             binder.ConstructProperties(mt, objects).Select(p => new LogEventProperty(p.Name, p.Value));
         }
 
-        [Benchmark]
-        public void CallPositionalConstructPropertiesWithoutExtraArgs()
+        [BenchmarkCategory("Positional"), Benchmark(Baseline = true)]
+        public void CallPositionalConstructPropertiesWithoutExtraArgsCheck()
         {
             var messageTemplate = "Hello {1} {0} nothing more";
             object[] objects = { 0, 1 };
@@ -35,7 +38,7 @@ namespace Serilog.PerformanceTests
             binder.ConstructPropertiesWithoutExtraArgs(mt, objects).Select(p => new LogEventProperty(p.Name, p.Value));
         }
 
-        [Benchmark]
+        [BenchmarkCategory("Named"), Benchmark]
         public void CallNamedConstructProperties()
         {
             var messageTemplate = "Hello {who} {what} nothing more";
@@ -44,8 +47,8 @@ namespace Serilog.PerformanceTests
             binder.ConstructProperties(mt, objects).Select(p => new LogEventProperty(p.Name, p.Value));
         }
 
-        [Benchmark]
-        public void CallNamedConstructPropertiesWithoutExtraArgs()
+        [BenchmarkCategory("Named"), Benchmark(Baseline = true)]
+        public void CallNamedConstructPropertiesWithoutExtraArgsCheck()
         {
             var messageTemplate = "Hello {who} {what} nothing more";
             object[] objects = { "who", "what" };
